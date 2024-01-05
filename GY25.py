1fda895dba14fa5c3346d7452b8f9d2b03be3028
IVEN-CN <13377529851@stu.gdou.edu.cn>
2024-01-06 00:00:06 +0800

    创建OLED的类，将读取的角度数据打印在OLED上

--- GY25.py
+++ GY25.py
@@ -1,7 +1,11 @@
-"""GY25陀螺仪读取角度测试文件"""
+"""GY25陀螺仪读取角度测试文件，并且显示在OLED上"""
 import serial
 import struct
 import os
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1306
+from PIL import ImageFont
 
 class GY25:
     def __init__(self, port='/dev/ttyS5', baudrate=115200):
@@ -22,11 +26,23 @@ class GY25:
                 yaw, pitch, roll = struct.unpack('<hhh', data[1:7])  # 解析数据包，<hhh表示3个16位整数
                 # 分别对应yaw、pitch、roll，数据类型为小端字节序，即低位在前，高位在后
                 return yaw / 100.0, pitch / 100.0, roll / 100.0  # 返回角度信息
-            
+
+class OLED():
+    def __init__(self) -> None:
+        # 创建一个I2C接口的实例
+        serial = i2c(port=1, address=0x36)
+        self.device = ssd1306(serial)
+        self.font = ImageFont.load_default()
+    def write_text(self, text):
+        with canvas(self.device) as draw:
+            draw.text((0, 0), text, font=self.font, fill="white")
+
 if __name__ == '__main__':
     gy25 = GY25()
+    oled = OLED()
     while True:
         yaw, pitch, roll = gy25.read_angle()
-        result = '偏航角:', yaw, '俯仰角:', pitch, '滚动角:', roll
         os.system('clear')
-        print(result)
+        print('偏航角:', yaw, '\n俯仰角:', pitch, '\n滚动角:', roll)
+        # 在画布上绘制文本
+        oled.write_text('偏航角: %s\n俯仰角: %s\n滚动角: %s' % (yaw, pitch, roll))
