e5bb67d81097ecf2c2e2e0c2a92c37df0271cba4
IVEN-CN <13377529851@stu.gdou.edu.cn>
2024-01-11 03:05:15 +0800

    使蓝牙连接更加可靠

--- bt.py
+++ bt.py
@@ -2,13 +2,15 @@ import bluetooth
 import wiringpi as wp       # 引入香橙派的wireingpi库
 from wiringpi import GPIO   # 引入香橙派的GPIO库
 from GY25 import OLED
+import serial
 
 class Bluetooth:
     """蓝牙连接类"""
-    def __init__(self,address='00:00:00:00:00:00'):
+    def __init__(self,address='00:00:00:00:00:00', ifserial=False):
         self.oled = OLED(3, 0x3C)
-        """初始化蓝牙连接"""
+        self.ifserial = ifserial
         self.oled.write_text('connecting...')
+        """初始化蓝牙连接"""
         while True:
             try:
                 self.serverMACAddress = address                         # 蓝牙模块的MAC地址
@@ -18,21 +20,40 @@ class Bluetooth:
                 break
             except:
                 continue
+        if self.ifserial:
+            while True:
+                try:
+                    self.ser = serial.Serial(port='/dev/rfcomm0', baudrate=9600)
+                    break
+                except:
+                    continue
         self.oled.clear()
         self.oled.write_text('connecting success!!')
 
         self.led = LED(25)                                      # 初始化LED指示灯
+
     def send(self, data):
         """发送数据"""
         self.led.on()
-        self.sock.send(data)
+        if self.ifserial == False:
+            self.sock.send(data)
+        else:
+            self.ser.write(data)
         self.led.off()
+        
     def read(self):
         """读取数据"""
-        return self.sock.recv(1024)                            # 一次最多读取1024字节
+        if self.ifserial == False:
+            return self.sock.recv(1024)                            # 一次最多读取1024字节
+        else:
+            return self.ser.read(1024)
+    
     def close(self):
         """关闭连接"""
-        self.sock.close()
+        if self.ifserial == False:
+            self.sock.close()
+        else:
+            self.ser.close()
 
 class LED:
     def __init__(self, pin):
